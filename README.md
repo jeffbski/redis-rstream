87f6688ebfd90d0202195ed62ef3510ba9c55e25
Vladislav Sevostyanov <vladislav.sevostyanov@kidl.no>
2019-10-30 13:32:39 +0300

    highWaterMark size limits fix

---
+++ README.md
@@ -0,0 +1,55 @@
+# redis-rstream
+
+redis-rstream is a node.js redis read stream which streams binary or utf8 data in chunks from a redis key using an existing redis client. (streams2) Tested with mranney/node_redis client.
+
+[![Build Status](https://secure.travis-ci.org/jeffbski/redis-rstream.png?branch=master)](http://travis-ci.org/jeffbski/redis-rstream)
+
+## Installation
+
+```bash
+npm install redis-rstream
+```
+
+You will also need the `redis` client (`npm install redis`) or other compatible library. You an also optionally install `hiredis` along with `redis` for additional performance.
+
+## Usage
+
+ - `redisRStream(client, key, [options])` - Construct a read stream instance by passing in `client`, redis `key`, and options. Be sure to enable an option in your redis client to return Buffers for the data, like `detect_buffers: true` so that binary data will be read properly. The default `options.chunkSize` (the size of the data packets in the stream) is 64KB, this is ignored if using the streams2 read(chunkSize) since the provided chunkSize will be used instead. You can limit how many pending reads are allowed for this read stream, by specifying `options.maxPendingReads` which defaults to 2. You also have the option to specify a range with `options.startOffset` (inclusive) and `options.endOffset` (non-inclusive), streaming only the chosen segment.
+
+```javascript
+var redis = require('redis');
+var redisRStream = require('redis-rstream'); // factory
+var client = redis.createClient(null, null, {detect_buffers: true}); // create client using your options and auth
+redisRStream(client, 'keyToStreamFrom')  // create instance of read stream w/default 64KB chunk size
+  .pipe(...)
+```
+
+Tested with mranney/node_redis client, but should work with any client that implements:
+
+ - `getrange(key, start, stop, cb)` - where key is a Buffer and the data returned is a Buffer
+
+
+## Goals
+
+ - Simple read stream which can use existing redis client (and especially mranney/node_redis)
+ - Remove all the complexity of managing a stream and reading in chunks from a redis key
+ - Create normal pausable node.js read stream which can be piped or used as any other stream
+ - uses streams2 from node 0.10+.
+
+## Why
+
+mranney/node_redis does not have direct ability to read a key as a stream, so rather than writing this logic again and again, wrap this up into a read stream so we simply point it to a key and it streams.
+
+Other redis stream implementations use their own direct network connections to redis, but I would prefer to use an existing connection for all my redis work which makes authentication and things lke failover easier to deal with.
+
+## Get involved
+
+If you have input or ideas or would like to get involved, you may:
+
+ - contact me via twitter @jeffbski  - <http://twitter.com/jeffbski>
+ - open an issue on github to begin a discussion - <https://github.com/jeffbski/redis-rstream/issues>
+ - fork the repo and send a pull request (ideally with tests) - <https://github.com/jeffbski/redis-rstream>
+
+## License
+
+ - [MIT license](http://github.com/jeffbski/redis-rstream/raw/master/LICENSE)
---
+++ test/basic.mocha.js
@@ -0,0 +1,110 @@
+/*global suite:false test:false */
+'use strict';
+
+var chai = require('chai-stack');
+var crypto = require('crypto');
+var redisRStream = require('..'); // require('redis-rstream');
+var redis = require('redis');
+var client = redis.createClient(null, null, { detect_buffers: true });
+
+
+var t = chai.assert;
+
+var KEY = 'foo';
+
+suite('basic');
+
+var TEST_DATA_SIZE = 2 * 1024 * 1024 + 25; // 2,025KB
+var TEST_DATA = crypto.randomBytes(TEST_DATA_SIZE);
+var EXPECTED_DIGEST = crypto.createHash('sha1').update(TEST_DATA).digest('base64');
+var START_OFFSET = 101;
+var LENGTH = 64*1024*2 + 5; // Two default chunks and 5 odd bytes
+var END_OFFSET = START_OFFSET + LENGTH;
+var EXPECTED_DIGEST2 = crypto.createHash('sha1').update(TEST_DATA.slice(START_OFFSET, END_OFFSET)).digest('base64');
+
+function setupDataForKey(cb) {
+  client.set(KEY, TEST_DATA, cb);
+}
+
+function cleanup(cb) {
+  client.del(KEY, cb);
+}
+
+before(function (done) { setupDataForKey(done); });
+after(function (done) { cleanup(done); });
+after(function () { client.quit(); })
+
+test('basic use streams2 binary data from key', function (done) {
+  var stream = redisRStream(client, KEY);
+  var accum = [];
+  stream
+    .on('error', function (err) { done(err); })
+    .on('readable', function () {
+      var data;
+      while ((data = stream.read()) !== null) {
+        accum.push(data);
+      }
+    })
+    .on('end', function () {
+      var allBuffer = Buffer.concat(accum);
+      var allDigest = crypto.createHash('sha1').update(allBuffer).digest('base64');
+      t.equal(allDigest, EXPECTED_DIGEST);
+      done();
+    });
+});
+
+test('basic use stream binary data from key, defaults to 64KB chunks', function (done) {
+  var stream = redisRStream(client, KEY);
+  var accum = [];
+  stream
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) {
+      accum.push(data);
+    })
+    .on('end', function () {
+      var allBuffer = Buffer.concat(accum);
+      var allDigest = crypto.createHash('sha1').update(allBuffer).digest('base64');
+      t.equal(allDigest, EXPECTED_DIGEST);
+      done();
+    });
+});
+
+test('basic use stream binary data from key and limited offset, defaults to 64KB chunks', function (done) {
+  var stream = redisRStream(client, KEY, {startOffset: START_OFFSET, endOffset: END_OFFSET});
+  var accum = [];
+  stream
+    .on('error', function (err) { done(err); })
+    .on('data', function (data) {
+      accum.push(data);
+    })
+    .on('end', function () {
+      var allBuffer = Buffer.concat(accum);
+      var allDigest = crypto.createHash('sha1').update(allBuffer).digest('base64');
+      t.equal(allBuffer.length, LENGTH);
+      t.equal(allDigest, EXPECTED_DIGEST2);
+      done();
+    });
+});
+
+
+
+test('all arguments missing for factory, throws error', function () {
+  function throwsErr() {
+    var stream = redisRStream();
+  }
+  t.throws(throwsErr, /RedisRStream requires client and key/);
+});
+
+test('client null, throws error', function () {
+  function throwsErr() {
+    var stream = redisRStream(null, KEY);
+  }
+  t.throws(throwsErr, /RedisRStream requires client and key/);
+});
+
+test('key null, throws error', function () {
+  function throwsErr() {
+    var stream = redisRStream(client, null);
+  }
+  t.throws(throwsErr, /RedisRStream requires client and key/);
+});
